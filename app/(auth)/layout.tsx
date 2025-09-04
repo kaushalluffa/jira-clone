baab15f94f688f1cb5b4b9c4cf4769592a0db016
kaushalluffa <kaushalluffa91@gmail.com>
2025-09-03 20:56:30 -0400

    feat: add sign up page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,17 @@
+'use client'
+import { useAuthSession } from "@/hooks/use-auth-hooks";
+import { useRouter } from "next/navigation";
+
 export default function AuthRootLayout({
   children,
 }: {
   children: React.ReactNode;
-}) {
+  }) {
+  const router = useRouter()
+  const session = useAuthSession()
+  if (session?.isAuthenticated) {
+   return router.push("/orgs")
+ }
+
   return <div className="grid min-h-screen place-items-center">{children}</div>;
 }
